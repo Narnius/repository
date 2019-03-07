5548287dff6d8f26803c885f5d4c5ebc0bec2310
artur.haack <artur.haack@r210-219.schule.local>
2019-03-07 10:40:35 +0100

    Harshadzahlrechner weitergeschrieben.

--- Uebungen_zu_Kontrollstrukturen/src/Harshadzahlrechner.java
+++ Uebungen_zu_Kontrollstrukturen/src/Harshadzahlrechner.java
@@ -1,11 +1,28 @@
-
+import java.util.ArrayList;
+import java.util.List;
 public class Harshadzahlrechner {
-
+	
+	static 
+	static int jot = 10;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arrayEins;
 	}
 
-	
+	public static void rechner()
+	{
+		while ( jot <= 100)
+		{
+			List<Integer> ziffernTrennung = new ArrayList<Integer>(); //Array, welches die Ziffern einzeln speichern soll.
+			ziffernTrennung.add(jot); 
+			jot = jot + 1;
+		}
+		
+		for (Integer i = 1; i <= 100; i++)
+		{
+			if ((2 % 3) == 0)
+		}
+	}
 
 }
