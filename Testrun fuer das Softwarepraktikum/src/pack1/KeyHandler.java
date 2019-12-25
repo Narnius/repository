3f7c501a1122878a935c87af033503f1fc93e756
Narnius <Narnius@Narnius.fritz.box>
2019-12-25 15:09:28 +0100

    Initial Commit

---
+++ Testrun fuer das Softwarepraktikum/src/pack1/KeyHandler.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class KeyHandler {
+
+	public KeyHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Testrun fuer das Softwarepraktikum/src/pack1/Var.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class Var {
+
+	public Var() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
